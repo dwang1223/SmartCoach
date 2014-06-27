cc9292a2f11b29809cc1b7ac38c826285e1cdb5d
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2014-06-26 21:58:59 -0400

    adding questions back

---
+++ SmartCoachDB/src/edu/wpi/smartcoach/model/ExerciseQuestions.java
@@ -0,0 +1,26 @@
+package edu.wpi.smartcoach.model;
+
+import edu.wpi.smartcoach.model.exercise.ExerciseState;
+
+public class ExerciseQuestions {
+
+	private static final String PLACEHOLDER_EXERCISE = "\\[exercise\\]";
+	private static final String PLACEHOLDER_EXERCISE_PRESENT = "\\[exercise-present\\]";
+	private static final String PLACEHOLDER_EXERCISE_CONTINUOUS = "\\[exercise-continuous\\]";
+	
+	private static final String PLACEHOLDER_TIME = "\\[time-in\\]";
+	private static final String PLACEHOLDER_LOCATION = "\\[location\\]";
+	
+	private static final String PLACEHOLDER_WEEK = "\\[week\\]";
+	
+	public static void prepareQuestion(OptionQuestionModel q, ExerciseState state){
+		
+		
+		
+	}
+	
+	private static String prepareString(String s, ExerciseState state){
+		return "";
+	}
+
+}
