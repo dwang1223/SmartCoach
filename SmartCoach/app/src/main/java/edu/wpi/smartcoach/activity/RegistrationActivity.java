54806a9378a218cb870e232f33ccabb67a8d8eff
chickenbellyfinn <chickenbellyfinn@gmail.com>
2014-12-26 17:51:59 -0500

    Convert project from Eclipse to Android Studio

---
+++ SmartCoach/app/src/main/java/edu/wpi/smartcoach/activity/RegistrationActivity.java
@@ -0,0 +1,175 @@
+package edu.wpi.smartcoach.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import edu.wpi.smartcoach.R;
+import edu.wpi.smartcoach.service.PatientProfile;
+import edu.wpi.smartcoach.service.WeightService;
+
+/**
+ * Activity that allows the user to enter in basic profile information
+ * @author Akshay
+ */
+public class RegistrationActivity extends Activity implements TextWatcher {
+
+	private static final String TAG = RegistrationActivity.class.getSimpleName();
+	
+	private EditText firstNameField, lastNameField;
+	private EditText heightFtField, heightInField, startWeightField, goalWeightField;
+	private Spinner genderSpinner;
+	
+	private Button submit;
+	
+	private boolean editing;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_registration);
+		
+		setTitle("SmartCoach Registration");
+		
+		if(getIntent().getExtras() != null){
+			editing = getIntent().getExtras().getBoolean("edit", false);
+		} else {
+			editing = false;
+		}
+		
+		submit = (Button) findViewById(R.id.submit);
+
+		submit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				submit();
+			}
+		});
+		
+		firstNameField = (EditText) findViewById(R.id.firstName);
+		lastNameField = (EditText) findViewById(R.id.lastName);
+		genderSpinner = (Spinner) findViewById(R.id.gender);
+		
+		heightFtField = (EditText)findViewById(R.id.height_ft);
+		heightInField = (EditText)findViewById(R.id.height_in);
+		startWeightField = (EditText)findViewById(R.id.current);
+		goalWeightField = (EditText)findViewById(R.id.goal);
+
+		firstNameField.addTextChangedListener(this);
+		startWeightField.addTextChangedListener(this);
+				
+		if(editing){ //if editing, get previous values
+			String firstName = PatientProfile.getFirstName(this);
+			String lastName = PatientProfile.getLastName(this);
+			String gender = PatientProfile.getGender(this);
+			
+			int heightInches = PatientProfile.getHeight(this);
+			float startWeight = PatientProfile.getStartWeight(this);
+			float goalWeight = PatientProfile.getGoalWeight(this);
+			
+			firstNameField.setText(firstName);
+			lastNameField.setText(lastName);
+			genderSpinner.setSelection((gender.equals("Male")?0:1));
+
+			if(heightInches > 0){
+				heightFtField.setText((heightInches/12)+"");
+				heightInField.setText((heightInches%12)+"");
+			}
+			startWeightField.setText(startWeight+"");
+			
+			if(goalWeight > 0){
+				goalWeightField.setText(goalWeight+"");
+			}
+			startWeightField.setEnabled(false);
+			
+			submit.setEnabled(true);
+			submit.setBackgroundResource(R.drawable.bg_card_highlight);
+			
+		} else {
+			submit.setEnabled(false);
+			submit.setBackgroundResource(R.drawable.bg_card_disable);	
+		}
+	}
+
+	/**
+	 * Save entered data and finish this activity
+	 */
+	private void submit() {
+		String firstName = firstNameField.getText().toString();
+		String lastName = lastNameField.getText().toString(); 
+		String gender = genderSpinner.getSelectedItem().toString();
+
+		float startWeight = Float.parseFloat(startWeightField.getText().toString());
+		
+		PatientProfile.setFirstName(firstName, this);
+		PatientProfile.setLastName(lastName, this);
+		PatientProfile.setGender(gender, this);
+		
+		PatientProfile.setStartWeight(startWeight, this);
+		
+		try {
+			int totalHeight = Integer.parseInt(heightFtField.getText().toString())*12 + Integer.parseInt(heightInField.getText().toString());
+			int goalWeight = (Integer.parseInt(goalWeightField.getText().toString()));
+
+			PatientProfile.setHeight(totalHeight, this);
+			PatientProfile.setGoalWeight(goalWeight, this);
+		} catch (Exception e){
+		}
+		
+		if(!editing){	
+			WeightService.getInstance().addWeight(System.currentTimeMillis(), startWeight, this);
+			
+			Intent intent = new Intent(this, ProfileActivity.class);
+			startActivity(intent);
+		}
+		
+		finish();
+	}
+	
+	@Override
+	public void afterTextChanged(Editable s) {
+		boolean firstValid = false;
+		boolean startValid = false;
+		
+		try {
+			Float.parseFloat(startWeightField.getText().toString());
+			startValid = true;
+		} catch (Exception e){
+			Log.d(TAG, "start not valid");
+			startValid = false;
+		}
+		
+		firstValid = !firstNameField.getText().toString().isEmpty();
+				
+		if(editing){
+			startValid = true;
+		}
+		
+		if(firstValid && startValid){
+			submit.setEnabled(true);
+			submit.setBackgroundResource(R.drawable.bg_card_highlight);
+		} else {
+			submit.setEnabled(false);
+			submit.setBackgroundResource(R.drawable.bg_card_disable);			
+		}
+	}
+	
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+	}
+
+
+}
---
+++ SmartCoach/app/src/main/java/edu/wpi/smartcoach/activity/SetReminderActivity.java
@@ -0,0 +1,150 @@
+package edu.wpi.smartcoach.activity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+import edu.wpi.smartcoach.R;
+import edu.wpi.smartcoach.reminders.Reminder;
+import edu.wpi.smartcoach.reminders.ReminderReciever;
+import edu.wpi.smartcoach.service.ReminderService;
+import edu.wpi.smartcoach.view.SetReminderFragment;
+
+public class SetReminderActivity extends FragmentActivity {
+	
+	private static final String TAG = SetReminderActivity.class.getSimpleName();
+
+	ReminderPagerAdapter mSectionsPagerAdapter;
+
+	ViewPager mViewPager;
+
+	String[] reminders;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+		setTitle("SmartCoach Set Reminders");
+		
+		reminders = getIntent().getExtras().getStringArray("reminder");
+		
+		for(int i = 0; i < reminders.length; i++){
+			if(reminders[i] == null){
+				reminders[i] = "";
+			}
+		}
+		
+		mSectionsPagerAdapter = new ReminderPagerAdapter(
+				getSupportFragmentManager());
+
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+	}
+	
+	private void doFinish(){
+			
+		for(int i = 0; i < mSectionsPagerAdapter.getCount(); i++){
+			SetReminderFragment f = (SetReminderFragment)mSectionsPagerAdapter.getItem(i);
+			
+			Reminder r = f.createReminder();
+			Integer[] reminderDays = f.getDayInts();
+			ReminderService.getInstance().addReminder(r, this);
+			
+			for(Integer d:reminderDays){
+				setAlarm(d, r.getHour(), r.getMinute(), r.getId());
+			}
+		}
+		
+		finish();
+		
+	}
+	
+	public void setAlarm(int dayOfWeek, int hour, int minute, int id) {
+		Calendar alarm = new GregorianCalendar();
+        alarm.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+
+        alarm.set(Calendar.HOUR_OF_DAY, hour);
+        alarm.set(Calendar.MINUTE, minute);
+        alarm.set(Calendar.SECOND, 0);
+
+        long alarmTime = alarm.getTimeInMillis();
+        //Also change the time to 24 hours.
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        
+        Intent intent = new Intent(this, ReminderReciever.class);
+        intent.putExtra("id", id);
+        PendingIntent pending = PendingIntent.getBroadcast(this, id, intent, 0);
+        
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, alarmTime, (long)(7*24*60*60*1000) , pending); 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+
+	public class ReminderPagerAdapter extends FragmentPagerAdapter {
+		
+		private HashMap<Integer, SetReminderFragment> frags;
+
+		public ReminderPagerAdapter(FragmentManager fm) {
+			super(fm);
+			frags = new HashMap<Integer, SetReminderFragment>();
+		}
+
+		@Override
+		public Fragment getItem(final int position) {
+			SetReminderFragment f;
+			if(!frags.containsKey(position)){
+				f = new SetReminderFragment();
+				f.setPosition(position, reminders.length);
+				f.setReminder(reminders[position]);
+				f.setListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						if(position != reminders.length-1){
+							mViewPager.setCurrentItem(position+1);
+						}else  {
+							doFinish();
+						}
+						
+					}
+				});
+				frags.put(position, f);
+			} else {
+				f = frags.get(position);
+			}
+			
+			return f;
+		}
+
+		@Override
+		public int getCount() {
+			return SetReminderActivity.this.reminders.length;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return SetReminderActivity.this.reminders[position];
+		}
+	}
+
+	
+
+
+}
